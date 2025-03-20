8b5677c9c304eda3721fc4cc76673809d7174127
Mina Ibrahim <eng.mina.hanna@hotmail.com>
2025-03-20 01:39:22 +0100

    Update requirements and startup script for Gunicorn configuration
    - Added new dependencies to requirements.txt including gunicorn and google-auth-httplib2.
    - Modified startup.sh to include a timeout and increase the number of workers for the Gunicorn server.

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd /home/site/wwwroot
-gunicorn main:app --bind=0.0.0.0:8000 --worker-class=uvicorn.workers.UvicornWorker
+gunicorn main:app --bind=0.0.0.0:8000 --worker-class=uvicorn.workers.UvicornWorker --timeout 600 --workers 4
